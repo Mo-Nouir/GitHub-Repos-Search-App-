deb34d3a9b2722871180dbfc9040b67f4438a23f
MoNouir <md.amine.nouir@gmail.com>
2022-11-29 01:03:22 +0100

    Coding the start component

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 export function Welcome(){
     return(
-       <h1>Welcome</h1>
+        <header className="grid place-items-center mt-10 mb-10">      
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">GitHub Repositories Search</h1>
+        <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here with our application you can search for github repos by name for each user.</p>
+    </header>
     )
 }
